221c9989c2f6472a600592f63093283519de7888
Zeth Thomas <zeththomas89@gmail.com>
2024-06-27 18:07:33 -0700

    git ignore set up

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
-    "host": "127.0.0.1",
+    "host": "localhost",
     "dialect": "postgres",
     "port": 5433
   },
